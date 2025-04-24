9969ac82fe1cd1c2474430de93be525b2d12f517
SeUz75 <seuz752@gmail.com>
2025-04-24 23:18:00 +0300

    Corrected azimuth and altitude variables

--- ArduinoCodeForServoMotors/ArduinoCodeForServoMotors.ino
+++ ArduinoCodeForServoMotors/ArduinoCodeForServoMotors.ino
@@ -1,7 +1,7 @@
 
 #include <AccelStepper.h>
 #include <TinyGPS++.h>
-#include <SoftwareSerial.h>
+
 #include <Servo.h>
 
 Servo azimuthServo; // Завъртаме ма хоризонталната ОС (азимут)
@@ -11,8 +11,7 @@ int azimudDegrees=0;
 int heightDegrees=0;
 
 
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
+
 const int ledPin=10; // Built-in LED on protoShield
 
 // The TinyGPS++ object
@@ -21,9 +20,9 @@ TinyGPSPlus gps;
 
 void setup(){
   Serial.begin(9600);
-  Serial1.begin(GPSBaud);
+  Serial1.begin(9600);
 
-  // Моторът за азимуд е в 9, и елевацията е в 10
+  // Моторът за азимуд е в 8, и елевацията е в 7
   azimuthServo.attach(7);
   heightServo.attach(8);
   pinMode(ledPin, OUTPUT);
@@ -34,28 +33,44 @@ void setup(){
 
 void loop(){
 
-  // This sketch displays information every time a new sentence is correctly encoded.
-  if (Serial1.available() > 0){
-    gps.encode(Serial1.read());
+  while (Serial1.available() > 0) {
+    char c = Serial1.read();
+    gps.encode(c);  // <--- Това липсваше
     if (gps.location.isUpdated()){
       Serial.print(gps.location.lat(), 6);
       Serial.print(',');
       Serial.println(gps.location.lng(), 6);
     }
-    delay(2000);
   }
 
 
+ // Обработка на входните данни за луната
  if (Serial.available()>0) {
       digitalWrite(ledPin,HIGH);
       delay(1000);
     // Read the incoming data
-    String receivedData = Serial.readStringUntil('\n');
-    int position = receivedData.toInt();
+    // String receivedData = Serial.readStringUntil('\n');
+    // int position = receivedData.toInt();
+
+    // -----------------------------
+     String moonsCoord = Serial.readStringUntil('\n'); // Четене на входен стринг
+    int commaIndex = moonsCoord.indexOf(','); // Намиране на запетаята
+
+    // Извличане на координати
+    String lontitudeString = moonsCoord.substring(0, commaIndex);
+    String azimuthString = moonsCoord.substring(commaIndex + 1);
+
+    // Преобразуване в float
+    int lontitude = lontitudeString.toInt();  // Височина на Луната
+    int azimuth = azimuthString.toInt(); // Азимут на Луната
+
+
+
+    //--------------------
     
     // Move servos
-    heightServo.write(position);
-    azimuthServo.write(position);
+    heightServo.write(lontitude);
+    azimuthServo.write(azimuth);
     delay(1000);
     //for(int i=0;i<position ; i++){
     //  digitalWrite(ledPin,HIGH);
